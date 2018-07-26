364a73d0f6d397eecfc0da7a3e2bd922021430ab
Sophie <sophie.collard@hotmail.fr>
2018-07-26 01:01:58 +0100

    Run while loop in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,20 +6,22 @@ import scala.io.StdIn
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Please provide a number between 0 and 999,999,999")
-    val number = StdIn.readInt()
+    while (true) {
+      println("Please provide a number between 0 and 999,999,999")
+      val number = StdIn.readInt()
 
-    try {
-      val spelledNumber = NumberSpeller(number).spell
-      spelledNumber match {
-        case Right(words) =>
-          println(s"$number is spelled '$words'")
-        case Left(e) =>
+      try {
+        val spelledNumber = NumberSpeller(number).spell
+        spelledNumber match {
+          case Right(words) =>
+            println(s"$number is spelled '$words'")
+          case Left(e) =>
+            println(e.getMessage)
+        }
+      } catch {
+        case e: IllegalArgumentException =>
           println(e.getMessage)
       }
-    } catch {
-      case e: IllegalArgumentException =>
-        println(e.getMessage)
     }
   }
 
